464de3d8b896da84118ec0497eddaaf2aa5169d7
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-10-13 15:03:17 -0700

    enable debug.

--- mhttc/settings.py
+++ mhttc/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 if os.environ.get("GAE_APPLICATION") is not None:
-    DEBUG = False
+    DEBUG = True
 
 # Custom user model
 AUTH_USER_MODEL = "users.User"
